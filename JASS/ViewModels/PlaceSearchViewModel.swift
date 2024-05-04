f4c20fd904038d38f9abb79f6a55aa65fda0e3ff
김기현 <creeps1104@gmail.com>
2024-05-04 19:31:01 +0900

    mod: searchResultsView 업데이트

--- JASS/ViewModels/PlaceSearchViewModel.swift
+++ JASS/ViewModels/PlaceSearchViewModel.swift
@@ -4,103 +4,117 @@ import GoogleMaps
 import CoreLocation
 
 struct SearchResults: Codable {
-   let results: [Place]
+  let results: [Place]
 }
 
 class PlaceSearchViewModel {
-   let provider = MoyaProvider<GooglePlacesAPI>()
-   var searchResults: [Place] = []
-   var isSearching: Bool = false
-   var updateSearchResults: (() -> Void)?
-   var showError: ((String) -> Void)?
-   let apiKey: String = "API_KEY"
+  let provider = MoyaProvider<GooglePlacesAPI>()
+  var searchResults: [Place] = []
+  var isSearching: Bool = false
+  var updateSearchResults: (() -> Void)?
+  var showError: ((String) -> Void)?
+  let apiKey: String = "API_KEY"
 
-   func searchPlacesNearCoordinate(_ coordinate: CLLocationCoordinate2D, radius: Double, types: [String], completion: @escaping ([Place]) -> Void) {
-       let location = "\(coordinate.latitude),\(coordinate.longitude)"
-//       let typeFilter = types.joined(separator: "|")
-       let parameters: [String: String] = [
-              "location": location,
-              "radius": "\(Int(radius))",
-              "type": types.joined(separator: "|"),
-              "key": Bundle.apiKey
-          ]
+  func searchPlacesNearCoordinate(_ coordinate: CLLocationCoordinate2D, radius: Double, types: [String], completion: @escaping ([Place]) -> Void) {
+      let location = "\(coordinate.latitude),\(coordinate.longitude)"
+      let parameters: [String: String] = [
+             "location": location,
+             "radius": "\(Int(radius))",
+             "type": types.joined(separator: "|"),
+             "key": Bundle.apiKey
+         ]
 
-       provider.request(.nearbySearch(parameters: parameters)) { result in
-           switch result {
-           case .success(let response):
-               do {
-                   let searchResults = try JSONDecoder().decode(SearchResults.self, from: response.data)
-                   print("API 호출 성공: \(searchResults.results.count)개의 장소 발견.")
+      provider.request(.nearbySearch(parameters: parameters)) { result in
+          switch result {
+          case .success(let response):
+              do {
+                  let searchResults = try JSONDecoder().decode(SearchResults.self, from: response.data)
+                  print("API 호출 성공: \(searchResults.results.count)개의 장소 발견.")
+                  completion(searchResults.results)
+              } catch {
+                  print("JSON 디코딩 오류: \(error)")
+                  completion([])
+              }
+          case .failure(let error):
+              print("API 요청 실패: \(error)")
+              completion([])
+          }
+      }
+  }
 
-                   completion(searchResults.results)
-               } catch {
-                   print("JSON 디코딩 오류: \(error)")
-                   completion([])
-               }
-           case .failure(let error):
-               print("API 요청 실패: \(error)")
-               completion([])
-           }
-       }
-   }
-
-    func searchPlacesInBounds(_ bounds: GMSCoordinateBounds, types: [String], completion: @escaping ([Place]) -> Void) {
-        let northeast = bounds.northEast
-        let southwest = bounds.southWest
-        let typeFilter = types.joined(separator: "|")
-        print("요청 types: \(types)") // 요청 types 출력
-
-        let parameters: [String: Any] = [
-            "key": Bundle.apiKey,
-            "northeast": "\(northeast.latitude),\(northeast.longitude)",
-            "southwest": "\(southwest.latitude),\(southwest.longitude)",
-            "type": typeFilter
-        ]
-
-        provider.request(.searchInBounds(parameters: parameters)) { result in
-           switch result {
-           case .success(let response):
-               do {
-                   let searchResults = try JSONDecoder().decode(SearchResults.self, from: response.data)
-                   print("API 응답 데이터: \(searchResults)") // 응답 데이터 출력
-                   completion(searchResults.results)
-               } catch {
-                   print("JSON 디코딩 오류: \(error)")
-                   completion([])
-               }
-           case .failure(let error):
-               print("API 요청 실패: \(error)")
-               completion([])
-           }
-       }
-   }
-
-   func searchPlace(input: String, completion: @escaping ([Place]) -> Void) {
-       guard !input.isEmpty else {
-           completion([])
-           return
-       }
+   func searchPlacesInBounds(_ bounds: GMSCoordinateBounds, types: [String], completion: @escaping ([Place]) -> Void) {
+       let northeast = bounds.northEast
+       let southwest = bounds.southWest
+       let typeFilter = types.joined(separator: "|")
+       print("요청 types: \(types)") // 요청 types 출력
 
        let parameters: [String: Any] = [
-           "key": apiKey,
-           "query": input
+           "key": Bundle.apiKey,
+           "northeast": "\(northeast.latitude),\(northeast.longitude)",
+           "southwest": "\(southwest.latitude),\(southwest.longitude)",
+           "type": typeFilter
        ]
-       print("검색 :\(parameters)")
 
-       provider.request(.textSearch(parameters: parameters)) { result in
-           switch result {
-           case .success(let response):
-               do {
-                   let searchResults = try JSONDecoder().decode(SearchResults.self, from: response.data)
-                   completion(searchResults.results)
-               } catch {
-                   print("JSON 디코딩 오류: \(error)")
-                   completion([])
-               }
-           case .failure(let error):
-               print("API 요청 실패: \(error)")
-               completion([])
-           }
-       }
-   }
+       provider.request(.searchInBounds(parameters: parameters)) { result in
+          switch result {
+          case .success(let response):
+              do {
+                  let searchResults = try JSONDecoder().decode(SearchResults.self, from: response.data)
+                  print("API 응답 데이터: \(searchResults)") // 응답 데이터 출력
+                  completion(searchResults.results)
+              } catch {
+                  print("JSON 디코딩 오류: \(error)")
+                  completion([])
+              }
+          case .failure(let error):
+              print("API 요청 실패: \(error)")
+              completion([])
+          }
+      }
+  }
+
+  func searchPlace(input: String, completion: @escaping ([Place]) -> Void) {
+      guard !input.isEmpty else {
+          completion([])
+          return
+      }
+
+      let parameters: [String: Any] = [
+          "key": apiKey,
+          "query": input
+      ]
+      print("검색 :\(parameters)")
+
+      provider.request(.textSearch(parameters: parameters)) { result in
+          switch result {
+          case .success(let response):
+              do {
+                  let searchResults = try JSONDecoder().decode(SearchResults.self, from: response.data)
+                  completion(searchResults.results)
+              } catch {
+                  print("JSON 디코딩 오류: \(error)")
+                  completion([])
+              }
+          case .failure(let error):
+              print("API 요청 실패: \(error)")
+              completion([])
+          }
+      }
+  }
+
+  func fetchPlacePhoto(reference: String, maxWidth: Int, completion: @escaping (URL?) -> Void) {  // 추가: fetchPlacePhoto 메서드 추가
+      provider.request(.photo(reference: reference, maxWidth: maxWidth)) { result in
+          switch result {
+          case .success(let response):
+              if let urlString = try? response.mapString(), let url = URL(string: urlString) {
+                  completion(url)
+              } else {
+                  completion(nil)
+              }
+          case .failure(let error):
+              print("사진 요청 실패: \(error)")
+              completion(nil)
+          }
+      }
+  }
 }
--- Service/GooglePlaceService.swift
+++ Service/GooglePlaceService.swift
@@ -2,69 +2,76 @@ import Moya
 import GoogleMaps
 
 enum GooglePlacesAPI {
-    case placeSearch(input: String)
-    case searchInBounds(parameters: [String: Any])
-    case nearbySearch(parameters: [String: String])
-    case textSearch(parameters: [String: Any])
+   case placeSearch(input: String)
+   case searchInBounds(parameters: [String: Any])
+   case nearbySearch(parameters: [String: String])
+   case textSearch(parameters: [String: Any])
+   case photo(reference: String, maxWidth: Int)  // 추가: 사진 API 추가
 }
 
 extension GooglePlacesAPI: TargetType {
-    var baseURL: URL {
-        return URL(string: "https://maps.googleapis.com/maps/api/place")!
-    }
+   var baseURL: URL {
+       return URL(string: "https://maps.googleapis.com/maps/api/place")!
+   }
 
-    var path: String {
-        switch self {
-        case .placeSearch:
-            return "/textsearch/json"
-        case .searchInBounds:
-            return "/nearbysearch/json"
-        case .nearbySearch:
-            return "/nearbysearch/json"
-        case .textSearch:
-            return "/textsearch/json"
-        }
-    }
+   var path: String {
+       switch self {
+       case .placeSearch:
+           return "/textsearch/json"
+       case .searchInBounds:
+           return "/nearbysearch/json"
+       case .nearbySearch:
+           return "/nearbysearch/json"
+       case .textSearch:
+           return "/textsearch/json"
+       case .photo:
+           return "/photo"  // 추가: 사진 경로 추가
+       }
+   }
 
-    var method: Moya.Method {
-        return .get
-    }
+   var method: Moya.Method {
+       return .get
+   }
 
-    var task: Task {
-        switch self {
-        case .placeSearch(let input):
-            return .requestParameters(parameters: [
-                "key": Bundle.apiKey,
-                "query": input
-            ], encoding: URLEncoding.queryString)
+   var task: Task {
+       switch self {
+       case .placeSearch(let input):
+           return .requestParameters(parameters: [
+               "key": Bundle.apiKey,
+               "query": input
+           ], encoding: URLEncoding.queryString)
+       case .photo(let reference, let maxWidth):  // 추가: 사진 요청 매개변수 추가
+           let parameters: [String: Any] = [
+               "maxwidth": maxWidth,
+               "photoreference": reference,
+               "key": Bundle.apiKey
+           ]
+           return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+       case .searchInBounds(let parameters):
+           var newParameters = parameters
+           newParameters["key"] = Bundle.apiKey
+           return .requestParameters(parameters: newParameters, encoding: URLEncoding.queryString)
+       case .nearbySearch(let parameters):
+           var newParameters = parameters
+           newParameters["key"] = Bundle.apiKey
+           return .requestParameters(parameters: newParameters, encoding: URLEncoding.queryString)
+       case .textSearch(let parameters):
+           var newParameters = parameters
+           newParameters["key"] = Bundle.apiKey
+           return .requestParameters(parameters: newParameters, encoding: URLEncoding.queryString)
+       }
+   }
 
-        case .searchInBounds(let parameters):
-            var newParameters = parameters
-            newParameters["key"] = Bundle.apiKey
-            return .requestParameters(parameters: newParameters, encoding: URLEncoding.queryString)
-
-        case .nearbySearch(let parameters):
-            var newParameters = parameters
-            newParameters["key"] = Bundle.apiKey
-            return .requestParameters(parameters: newParameters, encoding: URLEncoding.queryString)
-
-        case .textSearch(let parameters):
-            var newParameters = parameters
-            newParameters["key"] = Bundle.apiKey
-            return .requestParameters(parameters: newParameters, encoding: URLEncoding.queryString)
-        }
-    }
-
-    var headers: [String: String]? {
-        return ["Content-Type": "application/json"]
-    }
+   var headers: [String: String]? {
+       return ["Content-Type": "application/json"]
+   }
 }
 
 extension Bundle {
-    static var apiKey: String {
-        guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String else {
-            fatalError("API_KEY not found in .xcconfig file")
-        }
-        return apiKey
-    }
+   static var apiKey: String {
+       guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String else {
+           fatalError("API_KEY not found in .xcconfig file")
+       }
+       return apiKey
+   }
 }
